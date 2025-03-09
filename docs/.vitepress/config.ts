727b71d7c644c6f1244ec7aa77a79f09f480e013
Garrix <garrix@GarrixdeMacBook-Pro.local>
2025-03-09 21:50:14 +0800

    新增第一版Blog 分類和版型

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,24 +2,74 @@ import { defineConfig } from 'vitepress'
  
 export default defineConfig({
   base: '/blog/',
-  title: "Garrix CJ Blog",
+  title: "Bear Tsai",
   description: "記錄前端學習筆記",
   themeConfig: {
     nav: [
-      { text: "首頁", link: "/" },
-      { text: "文章", link: "/posts/" },
-      { text: "關於", link: "/about" }
+      { text: "Home", link: "/" },
+      { text: "Notes", link: "/notes/" },
+      { 
+        text: 'Skills', 
+        items: [
+          { text: 'Vue 基礎', link: '/notes/vue/init', },
+          { text: 'JS 基礎', link: '/notes/js/init', },
+          { text: 'TS 基礎', link: '/notes/ts/init', },
+          { text: 'Browser 基礎', link: '/notes/browser/init', },
+          { text: 'SEO 基礎', link: '/notes/seo/init', },
+        ]
+      },
+      { text: "About", link: "/about" },
     ],
     sidebar: {
-      "/posts/": [
+      "/notes": [
         {
-          text: "技術文章",
-          collapsible: true,
+          text: "",
+          collapsible: false,
           items: [
-            { text: "第一篇文章", link: "/posts/first-post" }
+            { text: '<span class="text-1">INTRO</span>', link: '/notes/' },
           ]
-        }
-      ]
-    }
+        },
+        {
+          text: 'Vue',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: 'Vue 基礎', link: '/notes/vue/init' },
+          ]
+        },
+        {
+          text: 'JS',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '基礎', link: '/notes/js/init' },
+          ]
+        },
+        {
+          text: 'TS',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '基礎', link: '/notes/ts/init' },
+          ]
+        },
+        {
+          text: '框架 & Browser',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '基礎', link: '/notes/browser/init' },
+          ]
+        },
+        {
+          text: 'SEO',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '基礎', link: '/notes/seo/init' },
+          ]
+        },
+      ],
+    },
   }
 });
---
+++ docs/notes/index.md
@@ -0,0 +1,6 @@
+# Intro  
+
+Hello！這裡是 **Bear Tsai 技術筆記**，  
+歡迎各位好手來交流～～ 🙌  
+
+![Bear Tsai 技術筆記](https://png.pngtree.com/png-vector/20221222/ourmid/pngtree-super-cute-cartoon-vector-bear-png-image_6504049.png)
